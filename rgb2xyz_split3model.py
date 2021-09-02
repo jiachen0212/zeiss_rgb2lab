c34e1e570437a94e6bd0ecafd45a07983295291a
chenjia <jiachen0212@163.com>
2021-09-02 19:34:21 +0800

    update

--- rgb2xyz_split3model.py
+++ rgb2xyz_split3model.py
@@ -74,6 +74,9 @@ with open(r'./all_data_lab.json', 'w') as js_file:
 #         print("test all size: {}, ok size: {}, percent: {}".format(c, d, d/c))
 #         scores.append(mean_squared_error(y_test, y_pred))
 def cross_val(X, Y, index, green_blue, rgb_ImgName, ff):
+    xyz_res = dict()
+    single_xyz_res = r'./xyz_{}.json'.format(index)
+
     X_train, X_test, y_train, y_test = TTS(X, Y, test_size=0.3, random_state=88)
 
     # 查看train中异常样本数量
@@ -83,7 +86,6 @@ def cross_val(X, Y, index, green_blue, rgb_ImgName, ff):
         if key_ in ["23_1", "23_2", "23_3", "23_4", "23_5"]:
             print(key_, '=====')
 
-    scores = []
 
     # hyperparameter_searching beat parameters
     parameters = json.load(open(r'./parameter_{}_{}.json'.format(index, green_blue), 'r'))
@@ -91,21 +93,35 @@ def cross_val(X, Y, index, green_blue, rgb_ImgName, ff):
     xgb_model = xgb.XGBRegressor(objective="reg:linear", **parameters)
     xgb_model.fit(X_train, y_train)
 
-    y_pred = xgb_model.predict(X_test)
+    # y_pred = xgb_model.predict(X_test)
+    # for index, item in enumerate(y_pred):
+    #     value = ''.join(str(int(a)) for a in X_test[index]) + str(y_test[index])
+    #     info = rgb_ImgName[value]
+    #     xyz_res[info] = str(item)
 
-    c, d = 0, 0
+    # test all data
+    y_pred = xgb_model.predict(X)
     for index, item in enumerate(y_pred):
-        # print("y_target: {:.3f}, y_predict: {:.3f}, abs_delta: {:.3f}".format(y_test[index], y_pred[index], abs(y_test[index] - y_pred[index])))
-        c += 1
-        if abs(y_test[index] - y_pred[index]) <= 0.5:
-            d += 1
-        else:
-            value = ''.join(str(int(a)) for a in X_test[index]) + str(y_test[index])
-            info = str(rgb_ImgName[value]) + '\tdiff: ' + str(abs(y_test[index] - y_pred[index]))
-            print(X_test[index], y_test[index], info)
-            ff.write(info + '\n')
-    print("test all size: {}, ok size: {}, percent: {}".format(c, d, d/c))
-    scores.append(mean_squared_error(y_test, y_pred))
+        value = ''.join(str(int(a)) for a in X[index]) + str(Y[index])
+        info = rgb_ImgName[value]
+        xyz_res[info] = str(item)
+
+
+    data = json.dumps(xyz_res)
+    with open(single_xyz_res, 'w') as js_file:
+        js_file.write(data)
+
+    # c, d = 0, 0
+    # for index, item in enumerate(y_pred):
+    #     c += 1
+    #     if abs(y_test[index] - y_pred[index]) <= 0.5:
+    #         d += 1
+    #     else:
+    #         value = ''.join(str(int(a)) for a in X_test[index]) + str(y_test[index])
+    #         info = str(rgb_ImgName[value]) + '\tdiff: ' + str(abs(y_test[index] - y_pred[index]))
+    #         print(X_test[index], y_test[index], info)
+    #         ff.write(info + '\n')
+    # print("test all size: {}, ok size: {}, percent: {}".format(c, d, d/c))
 
 
 def report_best_scores(results, index, green_blue, n_top=3):
@@ -198,6 +214,29 @@ def load_data(json_x, json_y, index, green_blue):
     return X, Y, rgb_ImgName
 
 
+def xyz2lab(x, y, z):
+    x /= 94.81211415
+    y /= 100
+    z /= 107.3369399
+    if y > 0.008856:
+        fy = np.power(y, 1/3)
+    else:
+        fy = 7.787 * y + 16 / 116.0
+    if x > 0.008856:
+        fx = np.power(x, 1/3)
+    else:
+        fx = 7.787 * x + 16 / 116.0
+    if z > 0.008856:
+        fz = np.power(z, 1/3)
+    else:
+        fz = 7.787 * z + 16 / 116.0
+    l = 116 * fy - 16
+    a = 500 * (fx - fy)
+    b = 200 * (fy - fz)
+    return [l, a, b]
+
+
+
 
 if __name__ == "__main__":
     # all data
@@ -205,7 +244,7 @@ if __name__ == "__main__":
     js_y = json.load(open(r'./all_data_lab.json', 'r'))
 
     # green: 0, blue: 1
-    green_blue = 1
+    green_blue = 0
 
     flags = ['x', 'y', 'z']
     ff = open(r'./bad_blue.txt', 'w')
@@ -223,13 +262,38 @@ if __name__ == "__main__":
         print('\n')
 
 
-        # 蓝绿样本划分
-        # for k, v in js_x.items():
-        #     v = [int(a) for a in v]
-        #     dir_index = int(k.split('_')[0])
-        #     # rgb 判断g值是否大于b值即可.
-        #     if v[1] > v[2]:
-        #         if dir_index > 21:
-        #             print(k, '==')
+    # 蓝绿样本划分
+    # for k, v in js_x.items():
+    #     v = [int(a) for a in v]
+    #     dir_index = int(k.split('_')[0])
+    #     # rgb 判断g值是否大于b值即可.
+    #     if v[1] > v[2]:
+    #         if dir_index > 21:
+    #             print(k, '==')
 
 
+    x_pred = json.load(open(r'./xyz_0.json', 'r'))
+    y_pred = json.load(open(r'./xyz_1.json', 'r'))
+    z_pred = json.load(open(r'./xyz_2.json', 'r'))
+    ks = list(x_pred.keys())
+    print(len(ks))
+    c = 0
+    for k in ks:
+        x, y, z = float(x_pred[k]), float(y_pred[k]), float(z_pred[k])
+        pre_l, pre_a, pre_b = xyz2lab(x, y, z)
+        real_l, real_a, real_b = js_y[k]
+        real_x, real_y, real_z = lab2xyz(real_l, real_a, real_b)
+        if abs(pre_l-real_l) <= 0.5 and abs(pre_a-real_a) <= 0.5 and abs(pre_b-real_b) <= 0.5:
+            c += 1
+    #     else:
+    #         print("diff_x: {}, diff_y: {}, diff_z:{}".format(abs(x - real_x), abs(y - real_y),  abs(z - real_z)))
+    #         print("diff_l: {}, diff_a: {}, diff_b:{}".format(abs(pre_l - real_l), abs(pre_a - real_a),
+    #                                                          abs(pre_b - real_b)))
+    #         print('\n')
+        elif abs(pre_l-real_l) >= 1 or abs(pre_a-real_a) >= 1 or abs(pre_b-real_b) >= 1:
+            print(k, js_y[k], pre_l, pre_a, pre_b)
+            print(k, real_x, real_y, real_z, x, y, z)
+            print("diff_x: {}, diff_y: {}, diff_z:{}".format(abs(x - real_x), abs(y - real_y), abs(z - real_z)))
+            print("diff_l: {}, diff_a: {}, diff_b:{}".format(abs(pre_l - real_l), abs(pre_a - real_a), abs(pre_b - real_b)))
+            print('\n')
+    print(c)
