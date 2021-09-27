7c8d5d2227165d1bce4dc64e8a6089f737060518
chenjia <jiachen0212@163.com>
2021-09-27 19:51:21 +0800

    change overlap0924.py

--- overlap0924.py
+++ overlap0924.py
@@ -8,108 +8,122 @@ import xlrd
 
 
 def imread(path):
-    # img = cv2.imread(path)
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     img = cv2.imdecode(np.fromfile(path, dtype=np.uint8), -1)
     img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
     return img
 
 
-def find_areas(img, color_lower1, color_lower2, color_upper, area_threshold):
+def cal_color_range(img, rect):
+    top_left = rect[0]
+    bottom_right = rect[2]
+    x1, y1 = top_left
+    x2, y2 = bottom_right
+
+    sub = img[y1:y2, x1:x2]
+    r, g, b = sub[:, :, 0], sub[:, :, 1], sub[:, :, 2]
+    mean_r = round(r.mean())
+    mean_g = round(g.mean())
+    mean_b = round(b.mean())
+
+    std_r = max(r.std(), 5)
+    std_g = max(g.std(), 5)
+    std_b = max(b.std(), 5)
+
+    r1, r2 = mean_r - std_r * 2, mean_r + std_r * 2
+    g1, g2 = mean_g - std_g * 2, mean_g + std_g * 2
+    b1, b2 = mean_b - std_b * 2, mean_b + std_b * 2
+
+    return (r1, g1, b1), (r2, g2, b2)
+
+
+def find_areas(img, color_lower, color_upper, area_threshold):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    mask1 = cv2.inRange(hsv, color_lower1, color_upper)
-    mask2 = cv2.inRange(hsv, color_lower2, color_upper)
-    mask1[:600] = 0
-    mask1[-600:] = 0
-    mask1[:, :600] = 0
-    mask1[:, -600:] = 0
-    mask2[:600] = 0
-    mask2[-600:] = 0
-    mask2[:, :600] = 0
-    mask2[:, -600:] = 0
-    colors = hsv[:, :, 2][mask1 != 0]
+    mask = cv2.inRange(hsv, color_lower, color_upper)
+    mask[:600] = 0
+    mask[-600:] = 0
+    mask[:, :600] = 0
+    mask[:, -600:] = 0
+    # cv2.imshow("mask", mask)
+    # cv2.waitKey()
+    # cv2.destroyAllWindows()
+    colors = hsv[:, :, 2][mask != 0]
     color = np.percentile(colors, 90) - 20
-    mask1 = (hsv[:, :, 2] > color).astype(np.uint8) * mask1
-
-    # cv2.imshow("mask1", mask1)
+    mask = (hsv[:, :, 2] > color).astype(np.uint8) * mask
+    # cv2.imshow("mask", mask)
     # cv2.waitKey()
     # cv2.destroyAllWindows()
-    #
-    # cv2.imshow("mask2", mask2)
-    # cv2.waitKey()
-    # cv2.destroyAllWindows()
-
-    contours, _ = cv2.findContours(mask1, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-    areas1 = []
+    contours, hier = cv2.findContours(mask, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+    areas = []
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area < area_threshold:
             continue
-        areas1.append(cnt)
-
-    contours, _ = cv2.findContours(mask2, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
-    areas2 = []
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area < area_threshold:
-            continue
-        areas2.append(cnt)
-
-    return areas1, areas2
+        areas.append(cnt)
+    return areas
 
 
-def cal_color(img, mask):
-    color = np.median(img[mask != 0], axis=0)
+def cal_color(img, area):
+    mask = np.zeros(img.shape[:2], np.uint8)
+    cv2.drawContours(mask, [area], 0, 1, -1)
+    color = img[mask != 0].mean(axis=0)
     return color
 
 
-def pipeline(img, color_lower1, color_lower2, color_upper, area_threshold):
+def is_ok(color, color_thresholds):
+    r, g, b = color
+    (r1, g1, b1), (r2, g2, b2) = color_thresholds
+
+    return r1 <= r <= r2 and g1 <= g <= g2 and b1 <= b <= b2
+
+
+def pipeline(img, color_lower, color_upper, area_threshold, color_thresholds):
     draw = img.copy()
-    areas1, areas2 = find_areas(img, color_lower1, color_lower2, color_upper, area_threshold)
-    if len(areas1) == 0 or len(areas2) == 0:  # 没找到满足条件的area，返回1
+    areas = find_areas(img, color_lower, color_upper, area_threshold)
+    if len(areas) == 0:  # 没找到满足条件的area，返回1
         return None, None, 1, draw
-    if len(areas1) > 1 or len(areas2) > 1:  # 找到多个满足条件的area，返回2
+    if len(areas) > 1:  # 找到多个满足条件的area，返回2
         return None, None, 2, draw
 
-    mask1 = np.zeros(img.shape[:2], np.float32)
-    mask2 = np.zeros(img.shape[:2], np.float32)
-    cv2.drawContours(mask1, areas1, 0, 1, -1)
-    cv2.drawContours(mask2, areas2, 0, 1, -1)
-    mask2 = mask2 - cv2.dilate(mask1, np.ones((7, 7), np.uint8))
-    mask2[mask2 < 0] = 0
+    moments = cv2.moments(areas[0])
+    cx = moments["m10"] / moments["m00"]
+    cy = moments["m01"] / moments["m00"]
+    half_height = 20
+    half_width = 20
+    area = np.array([
+        [cx - half_width, cy - half_height],
+        [cx - half_width, cy + half_height],
+        [cx + half_width, cy + half_height],
+        [cx + half_width, cy - half_height]
+    ], np.int32)
 
-    color1 = cal_color(img, mask1)
-    color2 = cal_color(img, mask2)
-    color = color1 - color2
+    color = cal_color(img, area)
+    cv2.drawContours(draw, [area], 0, (0, 0, 255), 2)
+    cv2.putText(draw, "color: {}".format(color), (100, 100), cv2.FONT_ITALIC, 3, (0, 255, 255), 2)
 
-    cv2.drawContours(draw, areas1, 0, (0, 0, 255), 1)
-    cv2.drawContours(draw, areas2, 0, (255, 0, 0), 1)
-    cv2.putText(draw, "color1: {}".format(color1), (100, 100), cv2.FONT_ITALIC, 3, (0, 255, 255), 2)
-    cv2.putText(draw, "color2: {}".format(color2), (100, 200), cv2.FONT_ITALIC, 3, (0, 255, 255), 2)
-    cv2.putText(draw, "color: {}".format(color), (100, 300), cv2.FONT_ITALIC, 3, (0, 255, 255), 2)
-
-    return color, "", 0, draw
-
+    if is_ok(color, color_thresholds):
+        cv2.putText(draw, "result: OK", (100, 200), cv2.FONT_ITALIC, 3, (0, 255, 255), 2)
+        return color, "OK", 0, draw
+    else:
+        cv2.putText(draw, "result: NG", (100, 200), cv2.FONT_ITALIC, 3, (0, 255, 255), 2)
+        return color, "NG", 0, draw
 
 def main():
     dir_color = dict()
     dirs = [i for i in range(1, 20)]
     for dir in dirs:
         directory = r"D:\work\project\卡尔蔡司AR镀膜\poc\20210924\20210924\{}".format(dir)
-        config_dir = r'D:\work\project\卡尔蔡司AR镀膜\poc\20210924_0927_updata_config\20210924\{}'.format(dir)
         paths = glob.glob(os.path.join(directory, "*.bmp"))
-        config_file = os.path.join(config_dir, "config.json")
+        config_file = os.path.join(directory, "config.json")
         with open(config_file) as f:
             conf = json.load(f)
-            color_lower1 = tuple(conf.get("color_lower1"))
-            color_lower2 = tuple(conf.get("color_lower2"))
+            color_lower = tuple(conf.get("color_lower"))
             color_upper = tuple(conf.get("color_upper"))
             area_threshold = conf.get("area_threshold")
+            color_thresholds = tuple(conf.get("color_thresholds"))
         for path in paths:
             im_name = int(path.split('\\')[-1][:-4])
             img = imread(path)
-            color, string, sig, draw = pipeline(img, color_lower1, color_lower2, color_upper, area_threshold)
-            print(color)
+            color, string, sig, draw = pipeline(img, color_lower, color_upper, area_threshold, color_thresholds)
             dir_color["{}_{}".format(dir + 50, im_name)] = color.tolist()
     data = json.dumps(dir_color)
     with open('./3float_rgb_0924.json', 'w') as js_file:
@@ -121,9 +135,9 @@ def main():
     for k, v in dir_color.items():
         if v[2] > v[1]:
             blue.append(k)
-            print(v)
         else:
             green.append(k)
+    assert len(blue)+len(green) == len(dir_color)
 
     return blue, green
 
@@ -167,7 +181,7 @@ def get_pre_dir(blue_dir_name):
 def split_blue_and_green(blue_dir_name, green_dir_name):
     color = json.load(open('./3float_rgb_0924.json', 'r'))
     lab = json.load(open('./lab_value_0924.json', 'r'))
-    assert len(color) == len(lab)
+    assert len(color) == 315
 
     # for blue
     blue_color = dict()
@@ -191,6 +205,7 @@ def split_blue_and_green(blue_dir_name, green_dir_name):
             green_color[k] = v
             green_lab[k] = lab[k]
 
+    print("蓝膜数量: {}, 绿膜数量: {}".format(len(blue_color), len(green_color)))
     # save json
     data = json.dumps(blue_color)
     with open('./blue_color.json', 'w') as js_file:
