b947808f8d79630ddc33b05dbdfba1b44ccf585d
chenjia <jiachen0212@163.com>
2021-10-15 14:29:50 +0800

    update rgb_lab check code

--- quick_check_rgb_lab_diff.py
+++ quick_check_rgb_lab_diff.py
@@ -90,49 +90,75 @@ def fun(data1_lab, data1_rgb):
 
 
 
-def base_blue_compare_green(data_lab, data_rgb, save_dir):
+def base_red_blue_compare_green(data_lab, data_rgb, save_dir):
     '''
     check 绿膜数据的lab rgb 一致性
     '''
 
     ff = open(os.path.join(save_dir, r'green_data_lab_rgb_check.txt'), 'w')
+    aa = open(os.path.join(save_dir, r'green_data_lab_rgb.txt'), 'w')
     labs = []
     rgbs = []
+    ks = []
     for k, v in data_lab.items():
         labs.append([float(a) for a in v])
         rgbs.append([float(a) for a in data_rgb[k]])
+        ks.append(k)
 
     a, b = 0, 0
+    bad_pair = []
     nums = len(labs)
     for i in range(nums):
         cur_lab = labs[i]
         cur_rgb = rgbs[i]
         tmp_lab = labs[:i] + labs[i + 1:]
         tmp_rgb = rgbs[:i] + rgbs[i + 1:]
+        tmp_ks = ks[:i] + ks[i + 1:]
         # a, b = 0, 0
         for j, tmp_rgb_ in enumerate(tmp_rgb):
-            # rgb的b值差值<1
-            if abs(tmp_rgb_[2]-cur_rgb[2]) < 1:
+            # rgb的rb值差值<1
+            if (abs(tmp_rgb_[2]-cur_rgb[2]) < 1) and (abs(tmp_rgb_[0]-cur_rgb[0]) < 1):
                 a += 1
                 tmp_lab_ = tmp_lab[j]
                 if ((tmp_rgb_[1] > cur_rgb[1]) and (tmp_lab_[1] < cur_lab[1]) or ((tmp_rgb_[1] < cur_rgb[1]) and (tmp_lab_[1] > cur_lab[1]))):
                     line1 = "cur_rgb: {}, cur_lab: {}".format(cur_rgb, cur_lab)
-                    print(line1)
+                    # print(line1)
                     line2 = "tmp_rgb: {}, tmp_lab: {}".format(tmp_rgb_, tmp_lab_)
-                    print(line2)
-                    print('\n')
-                    ff.write(line1 + '\n')
-                    ff.write(line2 + '\n')
-                    ff.write('\n')
+                    # print(line2)
+                    # print('\n')
+                    # ff.write(line1 + '\n')
+                    # ff.write(line2 + '\n')
+                    # ff.write('\n')
 
                     b += 1
+                else:
+                    bad_pair.append([ks[i], tmp_ks[j]])
     print("all blue similarity: {}, matched green: {}".format(a, b))
+    print(len(bad_pair))
+    slim_bad_pair = []
+    for i in range(len(bad_pair)):
+        slim_bad_pair.append(bad_pair[i])
+        for j in range(i, len(bad_pair)):
+            if bad_pair[i] == [bad_pair[j][1], bad_pair[j][0]]:
+                slim_bad_pair.remove(bad_pair[i])
+    print(len(slim_bad_pair))
+    # 输出不单调的lab和rgb
+    for k in slim_bad_pair:
+        line1 = "dir_name1: {}, lab: {}, rgb: {}".format(change_dir(k[0]), data_lab[k[0]], data_rgb[k[0]])
+        line2 = "dir_name1: {}, lab: {}, rgb: {}".format(change_dir(k[1]), data_lab[k[1]], data_rgb[k[1]])
+        # print(line1)
+        # print(line2)
+        # print('\n')
+        aa.write(line1 + '\n')
+        aa.write(line2 + '\n')
+        aa.write('\n')
 
 
 def change_dir(k):
     return str(int(k.split('_')[0])-14)+'_'+k.split('_')[1]
 
-def base_green_compare_blue(data_lab, data_rgb, save_dir):
+
+def base_green_red_compare_blue(data_lab, data_rgb, save_dir):
     '''
     check 蓝膜数据的lab rgb 一致性
     '''
@@ -159,8 +185,8 @@ def base_green_compare_blue(data_lab, data_rgb, save_dir):
         tmp_ks = ks[:i] + ks[i+1:]
         # a, b = 0, 0
         for j, tmp_rgb_ in enumerate(tmp_rgb):
-            # rgb的g值差值<1
-            if abs(tmp_rgb_[1]-cur_rgb[1]) < 1:
+            # rgb的rg值差值<1
+            if (abs(tmp_rgb_[1]-cur_rgb[1]) < 1) and (abs(tmp_rgb_[0]-cur_rgb[0]) < 1):
                 a += 1
                 tmp_lab_ = tmp_lab[j]
                 if ((tmp_rgb_[2] > cur_rgb[2]) and (tmp_lab_[2] < cur_lab[2]) or ((tmp_rgb_[2] < cur_rgb[2]) and (tmp_lab_[2] > cur_lab[2]))):
@@ -196,6 +222,62 @@ def base_green_compare_blue(data_lab, data_rgb, save_dir):
         aa.write('\n')
 
 
+
+def base_green_blue_check_red(data_lab, data_rgb, save_dir):
+    '''
+    RGB中gb相近, r值越大, LAB的A值越小
+    '''
+
+    aa = open(os.path.join(save_dir, r'RG相近_lab_a值检查.txt'), 'w')
+    labs = []
+    rgbs = []
+    ks = []
+    for k, v in data_lab.items():
+        labs.append([float(a) for a in v])
+        rgbs.append([float(a) for a in data_rgb[k]])
+        ks.append(k)
+
+    a, b = 0, 0
+    bad_pair = []
+    nums = len(labs)
+    for i in range(nums):
+        cur_lab = labs[i]
+        cur_rgb = rgbs[i]
+        tmp_lab = labs[:i] + labs[i + 1:]
+        tmp_rgb = rgbs[:i] + rgbs[i + 1:]
+        tmp_ks = ks[:i] + ks[i + 1:]
+        # a, b = 0, 0
+        for j, tmp_rgb_ in enumerate(tmp_rgb):
+            # rgb的gb值差值<1
+            if (abs(tmp_rgb_[2]-cur_rgb[2]) < 1) and (abs(tmp_rgb_[1]-cur_rgb[1]) < 1):
+                a += 1
+                tmp_lab_ = tmp_lab[j]
+                if ((tmp_rgb_[0] > cur_rgb[0]) and (tmp_lab_[0] > cur_lab[0]) or ((tmp_rgb_[0] <= cur_rgb[0]) and (tmp_lab_[0] <= cur_lab[0]))):
+                    b += 1
+                else:
+                    bad_pair.append([ks[i], tmp_ks[j]])
+    print("all green_blue similarity: {}, matched red: {}".format(a, b))
+    print(len(bad_pair))
+    slim_bad_pair = []
+    for i in range(len(bad_pair)):
+        slim_bad_pair.append(bad_pair[i])
+        for j in range(i, len(bad_pair)):
+            if bad_pair[i] == [bad_pair[j][1], bad_pair[j][0]]:
+                slim_bad_pair.remove(bad_pair[i])
+    print(len(slim_bad_pair))
+    # 输出不单调的lab和rgb
+    for k in slim_bad_pair:
+        line1 = "dir_name1: {}, lab: {}, rgb: {}".format(change_dir(k[0]), data_lab[k[0]], data_rgb[k[0]])
+        line2 = "dir_name1: {}, lab: {}, rgb: {}".format(change_dir(k[1]), data_lab[k[1]], data_rgb[k[1]])
+        # print(line1)
+        # print(line2)
+        # print('\n')
+        aa.write(line1 + '\n')
+        aa.write(line2 + '\n')
+        aa.write('\n')
+
+
+
 if __name__ == "__main__":
     # 针对蓝膜, 在g值相近的时候, rgb的b值越大则越蓝, 理论上应该对应出lab种的b值越负
     # 针对绿膜, 在b值相近的时候, rgb的g值越大则越绿, 理论上应该对应出lab种的a值越负
@@ -212,6 +294,7 @@ if __name__ == "__main__":
 
     # fun(data1_lab, data1_rgb)
 
-    # base_blue_compare_green(data1_lab, data1_rgb, save_dir)
-    base_green_compare_blue(data1_lab, data1_rgb, save_dir)
+    base_red_blue_compare_green(data1_lab, data1_rgb, save_dir)
+    base_green_red_compare_blue(data1_lab, data1_rgb, save_dir)
+    base_green_blue_check_red(data1_lab, data1_rgb, save_dir)
 
